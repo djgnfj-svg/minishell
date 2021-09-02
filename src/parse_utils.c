0f2af0c9fa7d9fdecd7168e1f88f23a5e38cb2fe
djgnfj-svg <djgnfj8923@naver.com>
2021-09-03 08:08:35 +0900

    parse_utils.c make

--- src/parse_utils.c
+++ src/parse_utils.c
@@ -6,7 +6,7 @@
 /*   By: ysong <ysong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 06:40:11 by ysong             #+#    #+#             */
-/*   Updated: 2021/09/03 07:18:00 by ysong            ###   ########.fr       */
+/*   Updated: 2021/09/03 08:08:13 by ysong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ static void *init_data(char *input)
 
 t_data parse_data(char *input)
 {
+	// TO-DO 파이프라인을 적용하면 |을 기준으로 명령어를 나눈다. 
 	t_data *data;
 	init_data(data, input);
 
