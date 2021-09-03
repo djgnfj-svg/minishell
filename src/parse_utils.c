5fcae94829d64c279ce4afa7bd364e20bea7c7e9
djgnfj-svg <djgnfj8923@naver.com>
2021-09-04 04:04:15 +0900

    23

--- src/parse_utils.c
+++ src/parse_utils.c
@@ -20,6 +20,18 @@
 
 static void init_cmd(char *input, t_data *data)
 {
+	// - 받을때 까지 입력을 받고?
+	// 아토이를 해서 받은 값이 명령어이면 추가
+	// -? 이후의 값? 만약에 -가 없으면
+	// 명령어만 추가 되는가?
+	/*
+	그러면 echo -1234이면
+	어떡할 것인가?
+	공백을 기준으로 쪼겠을때 [0]에 들어 있는 것이
+	cmd이고 뒤에 있는 것이 옵션으로 저장된다.
+	*/
+	char *temp = ft_split(input, ' ');
+	
 	data->cmd = input;
 }
 t_data *parse_data(char *input)
